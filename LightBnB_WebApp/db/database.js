b0de04d9c2f4e7136273f47a6061d09e8d80e90f
manavpanchotiya <manavpanchotiya@gmail.com>
2025-05-02 14:56:08 -0500

    merged min and max price

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -124,14 +124,10 @@ const getAllProperties = (options, limit = 10) => {
     queryString += `AND owner_id = $${queryParams.length}`;
   }
 
-  if (options.minimum_price_per_night) {
-    queryParams.push((options.minimum_price_per_night) * 100);
-    queryString += `AND cost_per_night >= $${queryParams.length}`;
-  }
-
-  if (options.maximum_price_per_night) {
-    queryParams.push((options.maximum_price_per_night) * 100);
-    queryString += `AND cost_per_night <= $${queryParams.length}`;
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100);
+    queryParams.push(options.maximum_price_per_night * 100);
+    queryString += `AND (cost_per_night >= $${queryParams.length - 1} AND cost_per_night<= $${queryParams.length})\n`;
   }
 
   queryString += `GROUP BY properties.id`;
